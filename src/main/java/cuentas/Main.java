946b326fa38d6efb524f2e04e4f326971c6a61cc
wirot <wirot@192.168.1.112>
2021-02-18 16:41:58 +0100

    Usar el parámetro cantidad con el método retirar

---
+++ src/main/java/cuentas/Main.java
@@ -0,0 +1,32 @@
+package cuentas;
+
+
+import cuentas.CCuenta;
+
+public class Main {
+
+    public static void main(String[] args) {
+        operativa_cuenta(50);
+    }
+
+    private static void operativa_cuenta(float cantidad) {
+        CCuenta cuenta1;
+        double saldoActual;
+
+        cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        saldoActual = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldoActual );
+
+        try {
+            cuenta1.retirar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            System.out.println("Ingreso en cuenta");
+            cuenta1.ingresar(cantidad);
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+}
